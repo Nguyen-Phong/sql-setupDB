513d62be2a82d370ee8b206a7b0724f983f41208
unknown <nguyennhuphong2525@gmail.com>
2021-08-05 08:39:42 +0700

    first commit

---
+++ bookTypes.sql
@@ -0,0 +1,28 @@
+create table books.bookTypes (
+	id varchar(10),
+    bookNames varchar(50),
+    publisher varchar(50)
+);
+
+insert into books.bookTypes (id, bookNames, publisher)
+values ("TN1", "Math", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("HM2", "Literature", "Dong Thap");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("TN3", "Physics", "Ha Noi");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("VH4", "Painting", "Thieu Nhi");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("TL5", "Swimming", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("ST6", "Skateboarding", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("CM7", "Chemistry", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("EG8", "English", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("SC9", "Science", "Kim Dong");
+insert into books.bookTypes (id, bookNames, publisher)
+values ("PY10", "physical education", "Kim Dong");
+
+select *from books.bookTypes;
